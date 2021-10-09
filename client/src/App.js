3253f0e52ee493865574d43ecba17d87e3dc4434
Yannick van Dijk <YannickvDijk@gmail.com>
2021-10-09 14:46:56 +0200

    Added row selection

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { PaginationTable } from './components/PaginationTable';
+import { RowSelection } from './components/RowSelection';
 
 function App() {
 	return (
 		<div className='App'>
-			<PaginationTable />
+			<RowSelection />
 		</div>
 	);
 }
